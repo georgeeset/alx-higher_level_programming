9e14e54c1a99059ba19853bed4fc0c9a9b8a0a8a
georgeeset <georgeperfect4u@gmail.com>
2022-12-01 00:51:15 +0100

    new line thing added

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    print("{:02}".format(i), end='')
     if i < 99:
-        print(", ", end='')
+        print("{:02}".format(i), end=', ')
+    else:
+        print("{}".format(i))
