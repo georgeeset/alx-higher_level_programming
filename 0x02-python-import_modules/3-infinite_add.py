999b99eb341c7170f382e5ebb1e3d3914d6a1b3a
georgeeset <georgeperfect4u@gmail.com>
2022-12-02 04:52:10 +0100

    infinite addition, pycodestyle

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,10 +2,10 @@
 import sys
 
 if __name__ == "__main__":
-    l = len(sys.argv) - 1
+    argsCount = len(sys.argv) - 1
     value = 0
 
-    if l > 0:
+    if argsCount > 0:
         for item in sys.argv:
             if item == sys.argv[0]:
                 continue
