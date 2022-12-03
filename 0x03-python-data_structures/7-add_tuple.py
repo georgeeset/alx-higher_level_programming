e84b6fdee8c8a19c3b11f276e10410f1238bc2b5
georgeeset <georgeperfect4u@gmail.com>
2022-12-03 16:02:05 +0100

    Tuples addition: indentation fixed

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -19,13 +19,13 @@ def add_tuple(tuple_a=(), tuple_b=()):
         a1 = tuple_a[0]
         a2 = tuple_a[1]
 
-        if len_b == 0:
+    if len_b == 0:
             b1 = 0
             b2 = 0
-        elif len_b == 1:
+    elif len_b == 1:
             b1 = tuple_b[0]
             b2 = 0
-        else:
+    else:
             b1 = tuple_b[0]
             b2 = tuple_b[1]
 
