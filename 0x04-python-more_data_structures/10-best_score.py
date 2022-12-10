f2794e9c369ed86836ce32400891771c3eceb611
georgeeset <georgeperfect4u@gmail.com>
2022-12-10 12:57:59 +0100

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,9 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return (None)
+
     bestScore = list(a_dictionary.values())[0]
+    bestKey = list(a_dictionary.keys())[0]
     for i, j in a_dictionary.items():
         if j > bestScore:
             bestScore = j
