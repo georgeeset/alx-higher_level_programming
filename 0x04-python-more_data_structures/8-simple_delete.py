fb7c3cced2c9ab0e17e8b0e410e43996f14d260a
georgeeset <georgeperfect4u@gmail.com>
2022-12-10 11:24:11 +0100

    simple delete by key

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def simple_delete(a_dictionary, key = ""):
+    key_set = set(a_dictionary.keys())
+    if key in key_set:
+        a_dictionary.pop(key)
+    return (a_dictionary)
