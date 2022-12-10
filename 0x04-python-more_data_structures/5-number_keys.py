2de651c3a59e33ad33091eb09b74a20fbe58de5d
georgeeset <georgeperfect4u@gmail.com>
2022-12-10 08:44:53 +0100

    Dont use len, count every item

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     num = 0
-    list_keys = list(a_dictionary.keys())
+    list_keys = a_dictionary.keys()
 
     for i in list_keys:
         num += 1
