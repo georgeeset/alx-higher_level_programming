fdf34ddc7b5f5b2ccdaf0de0e35a887321cd2a56
georgeeset <georgeperfect4u@gmail.com>
2022-12-20 23:03:29 +0100

    exception error specified

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,8 +6,8 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end='')
-        except:
-            break
+        except Exception:
+            pass
         else:
             count += 1
 
