73834c0751a6ba4fdf184a5eb791b25c3d01464c
georgeeset <georgeperfect4u@gmail.com>
2022-12-25 16:31:22 +0100

    singly linked list

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+
+class Node():
+    """ A node class of singly linked list"""
+
+
+    def __init__(self, data, next_node=None, ):
+        """
+        data: contains integer
+        next_node: link to next Node connected
+        to the current node
+        """
+        if not isinstance(data, int):
+            raise TypeError("data must be an integer")
+        else:
+            self._data = data
+
+        if isinstance(next_node, Node) or (next_node == None):
+            self._next_node = next_node
+        else:
+            raise TypeError("next_node must be a Node object")
+
+    @property
+    def data(self):
+        """ Retrieve data """
+        return self._data
+
+    @data.setter
+    def data(self, value):
+        """ Change the value of Data"""
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        else:
+            self._data = value
+
+    @property
+    def next_node(self):
+        """ Retrieve value of next_node"""
+        return self._next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """ update the value of next_node """
+        if isinstance(value, Node) or (value == None):
+            self._next_node = value
+        else:
+            raise TypeError("next_node must be a Node object")
+
+class SinglyLinkedList():
+    """ this vlass defines a singly linked list"""
+
+
+    def __init__(self,):
+        self._head = None
+
+    def __str__(self):
+        """ convert the content of singly linked
+        list to string
+        """
+        fullstr = ""
+        node = self._head
+        while node != None:
+            fullstr += f"{node.data}\n"
+            node = node.next_node
+        return fullstr[:-1]
+
+    def sorted_insert(self, value):
+        """Inserts a node in sorted link list"""
+
+        new_node = Node(value)
+        if self._head == None:
+            self._head = new_node
+            return
+
+        if value < self._head.data:
+            new_node.next_node = self._head
+            self._head = new_node
+            return
+
+        point = self._head
+        while point.next_node and point.next_node.data < value:
+            point = point.next_node
+        new_node.next_node = point.next_node
+        point.next_node = new_node
