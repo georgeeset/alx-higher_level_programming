54630aea2c7ea4e6fda06532ab860c647b399bf5
georgeeset <georgeperfect4u@gmail.com>
2022-12-31 10:47:26 +0100

    methond description added

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -6,9 +6,18 @@ Module to print square
 
 
 def print_square(size):
+    """ Print a square with the character #
+    Args:
+        @size: the size length of the square
+
+    Return:
+        Returns nothing
+
+    Exceptions:
+        TypeError: if value is not an integer
+        TypeErro: if value of size is less than 0
+    """
     if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
