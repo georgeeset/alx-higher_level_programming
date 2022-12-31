c0c9fdf4eb29731956e7330ac6e025421330e12e
georgeeset <georgeperfect4u@gmail.com>
2022-12-31 10:25:02 +0100

    3-print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Module to print square
+"""
+
+
+def print_square(size):
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(0, size):
+        for j in range(0, size):
+            print('#', end='')
+        print()
