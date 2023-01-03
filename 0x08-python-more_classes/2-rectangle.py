dc2fff17d2977049094623236e2b1ed885f425d8
georgeeset <georgeperfect4u@gmail.com>
2023-01-03 12:32:59 +0100

    check data at init

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -13,7 +13,16 @@ class Rectangle:
             width: width of the rectangle
             height: height of the rectangle
         """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
         self._width = width
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self._height = height
 
     @property
