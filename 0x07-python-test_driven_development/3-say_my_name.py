4131b7a943669e6f1edede411589b5ca89e3b960
georgeeset <georgeperfect4u@gmail.com>
2023-01-06 21:44:46 +0100

    string instance using if type() is not str

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -18,9 +18,9 @@ def say_my_name(first_name, last_name=""):
     Exceptions:
        TypeError: if either first_name or last_name is not a string
     """
-    if not isinstance(first_name, str):
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
 
     print("My name is {} {}".format(first_name, last_name))
