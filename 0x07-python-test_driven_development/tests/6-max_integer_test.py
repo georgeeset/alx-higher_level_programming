d5f921008ac761007bd8d870018c636011dfc480
georgeeset <georgeperfect4u@gmail.com>
2023-01-07 01:40:42 +0100

    tests rewritten in class with each test as function

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,21 +8,40 @@ estifor max_integer function
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+class TestMaxInteger(unittest.TestCase):
+    """
+    Test the max_integer function
+    """
+    def test_max_integer(self):
+        result = max_integer([1, 4, 3, 2])
+        self.assertEqual(result, 4)
+        result = max_integer([12, 9, 7, 2])
+        self.assertEqual(result, 12)
 
-class TestMaxIntegerMethod(unittest.TestCase):
+    def test_isint(self):
+        var = 1
+        self.assertTrue(max_integer([var, 2]) == 2)
+        x = 5
+        self.assertTrue(max_integer([1, x]) == x)
 
-    """Method test for max_integer function"""
+    def test_one_entry(self):
+        self.assertEqual(max_integer([10]), 10)
 
-    def test_data_types(self):
+    def test_negative_integer(self):
+        self.assertEqual(max_integer([-1, -2, -5]), -1)
+
+    def test_no_argument(self):
+        self.assertEqual(max_integer(), None)
+
+    def test_same_entry(self):
+        a = 50
+        self.assertEqual(max_integer([a, a, a, a]), a)
+
+    def test_float_integer(self):
+        self.assertEqual(max_integer([4.0, 3, 2]), 4.0)
+
+    def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
-        self.assertEqual(max_integer([0]), 0)
-        self.assertEqual(max_integer((2, 4, 12)), 12)
-        self.assertEqual(max_integer(['a', 'A', 'c', 'C']), 'c')
-        self.assertEqual(max_integer([43, 100, 535, 22, 655, 7, 112]), 655)
 
-    def test_same_number(self):
-        self.assertTrue(max_integer([7, 7, 7, 7]) == 7)
-
-    def test_exceptions(self):
-        with self.assertRaises(TypeError):
-            max_integer(7)
+        if __name__ == '__main__':
+            unittest.main()
