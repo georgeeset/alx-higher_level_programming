524b1f95aee803243e1c8a4bec4f690e0e31c822
georgeeset <georgeperfect4u@gmail.com>
2023-01-11 16:48:23 +0100

    type method added

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -12,5 +12,6 @@ def inherits_from(obj, a_class):
     Return:
         bool - True if obj is an instance of a_class, False otherwise
     """
-
+    if type(obj) is a_class:
+        return False
     return isinstance(obj, a_class)
