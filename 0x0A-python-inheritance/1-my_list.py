c1f68e774708763a91e8ead542bd94d5a5b16fa9
georgeeset <georgeperfect4u@gmail.com>
2023-01-11 16:11:03 +0100

    stndardized my_list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-
 """
-module contains method A class MyList
+module contains a class MyList which is ment to work as a
+mustom list
 """
 
 
 class MyList(list):
     """
-    This class that inherites for list
+    This is a custum class that inherites from list class
     """
+
+    def __init__(self, *argc, **argv):
+        """
+        initialize the instance of myList class
+        """
+        list.__init__(self, *argc, **argv)
 
     def print_sorted(self):
         """
