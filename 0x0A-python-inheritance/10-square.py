ee2934f17a2fb4aade069ff067c822acab7745f1
georgeeset <georgeperfect4u@gmail.com>
2023-01-11 23:33:10 +0100

    square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module contains class Square"""
+
+
+class Square(__import__('9-rectangle').Rectangle):
+    """Square Class"""
+
+    def __init__(self, size):
+        """Initializes the square"""
+
+        super().integer_validator('size', size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Calculates and returns area"""
+        return self.__size * self.__size
