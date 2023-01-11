077319161f05c8145810a96d2ebf7e1b86656623
georgeeset <georgeperfect4u@gmail.com>
2023-01-11 16:45:56 +0100

    4.only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""the object is an instance of a class that
+inherited (directly or indirectly) from the specified class
+"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if an object inherits from a_class
+    Args:
+        obj - object to be checked
+        a_class - class to compare
+    Return:
+        bool - True if obj is an instance of a_class, False otherwise
+    """
+
+    return isinstance(obj, a_class)
