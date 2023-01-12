404d2d5c890774bea0e035ad7216ec39ff7869b8
georgeeset <georgeperfect4u@gmail.com>
2023-01-12 04:31:02 +0100

    only run on main

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,17 +3,17 @@
 save them to a file
 """
 import sys
-import os.path
 
 
 save_file = __import__('5-save_to_json_file').save_to_json_file
 load_file = __import__('6-load_from_json_file').load_from_json_file
 
 my_list = []
-if os.path.exists("add_item.json"):
-    my_list = load_file("add_item.json")
-
-    for arg in sys.argv[1:]:
-        my_list.append(arg)
-
-    save_file(my_list, "add_item.json")
+if __name__ == '__main__':
+    try:
+        lst = loadfile("add_item.json")
+    except:
+        lst = []
+    for i in range(1, len(sys.argv)):
+        lst.append(sys.argv[i])
+    savefile(lst, "add_item.json")
