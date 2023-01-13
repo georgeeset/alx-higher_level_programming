6f2ff5445897d588cff5022475845a64d9573d0d
georgeeset <georgeperfect4u@gmail.com>
2023-01-13 03:56:03 +0100

    class to json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""this module contains a function that returns a dictionary
+descriptionwith simple data structure
+"""
+
+def class_to_json(obj):
+    """ converts an object to a JSON drepresentation
+    args:
+        obj: is an instance of a clas
+    return:
+        returns dictionary description of object
+    """
+    return (obj.__dict__)
