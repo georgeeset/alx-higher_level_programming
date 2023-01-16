df577b47060a905fd75a9d4bc1f75b2f6be5f332
georgeeset <georgeperfect4u@gmail.com>
2023-01-16 08:19:20 +0100

    Dictionary to instance class method added to create instance from dictioanry

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -52,3 +52,13 @@ class Base:
         if json_string is None or len(json_string) == 0:
             return new_list
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attribures already set"""
+        if cls.__name__ == "Rectangle":
+            new = cls(10, 10)
+        else:
+            new = cls(10)
+        new.update(**dictionary)
+        return (new)
