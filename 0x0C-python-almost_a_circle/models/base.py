789c25a0b28151c421be51498e86be950dcf454e
georgeeset <georgeperfect4u@gmail.com>
2023-01-16 07:28:27 +0100

    dictionary to json added to base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ This module contains the Base class"""
 
+import json
+
+
 
 class Base:
     """Base class with private class attributes and constructors"""
@@ -14,3 +17,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    
