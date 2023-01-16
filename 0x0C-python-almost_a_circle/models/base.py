372ef506dfc28951123ad6fcf380e99b5bfe29f7
georgeeset <georgeperfect4u@gmail.com>
2023-01-16 08:09:55 +0100

    Json string to dictionary static method added to base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,10 +27,28 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """writes the json string representation of list_objs to a file"""
+        """writes the json string representation of list_objs to a file
+        args:
+            cls: Which ever class you need to save
+            lst_objs; list of class instances you want to save
+        """
         new_list = []
         if list_objs:
-            for i in list_objs:
-                new_list.append(cls.to_dictionary(i))
+            for inst in list_objs:
+                new_list.append(cls.to_dictionary(inst))
             with open("{}.json".format(cls.__name__), 'w') as f:
                 f.write(cls.to_json_string(new_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ Returns the list of the JSON string representation json_string
+        args:
+            json_string: is a string representing a list of dictionaries
+        return:
+            Empty list if Json_string is None or empty
+            returns the list represented by json_string
+        """
+        new_list = []
+        if json_string is None or len(json_string) == 0:
+            return new_list
+        return json.loads(json_string)
