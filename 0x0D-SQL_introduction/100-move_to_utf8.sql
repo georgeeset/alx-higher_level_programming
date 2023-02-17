e933b8b73c90342ce10e739b5dee41881708f919
georgeeset <georgeperfect4u@gmail.com>
2023-02-17 14:54:11 +0100

    Set db atain before converting table

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,6 +2,7 @@
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET = utf8mb4 COLLATE utf8mb4_unicode_ci;
 --convert first_table
+USE hbtn_0c_0;
 ALTER TABLE hbtn_0c_0.first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
