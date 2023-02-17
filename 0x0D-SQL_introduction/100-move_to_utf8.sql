5f2e57b68e50ce1461112f68f77360020da2effe
georgeeset <georgeperfect4u@gmail.com>
2023-02-17 14:47:22 +0100

    temperatures and goto utf8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,7 @@
+-- convert to utf-8
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET = utf8mb4 COLLATE utf8mb4_unicode_ci;
+--convert first_table
+ALTER TABLE hbtn_0c_0.first_table
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
---
+++ 0x0D-SQL_introduction/108-avg_temperatures.sql
@@ -0,0 +1,4 @@
+-- Write a script that displays the average temperature
+SELECT city, AVG(value) AS "avg_temp"
+FROM temperatures GROUP BY city
+ORDER BY avg_temp DESC;
