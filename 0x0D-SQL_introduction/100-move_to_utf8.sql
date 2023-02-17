188d9465f15c6709de4fd2161d508846bd238ac8
georgeeset <georgeperfect4u@gmail.com>
2023-02-17 14:56:37 +0100

    rearanged and comment removed

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,5 @@
 -- convert to utf-8
-ALTER DATABASE hbtn_0c_0
-CHARACTER SET = utf8mb4 COLLATE utf8mb4_unicode_ci;
---convert first_table
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
-ALTER TABLE hbtn_0c_0.first_table
-CONVERT TO CHARACTER SET utf8mb4
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
