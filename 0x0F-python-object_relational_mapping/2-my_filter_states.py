723d2e5beb076dab6f26957a6fac0030a66f55b5
georgeeset <georgeperfect4u@gmail.com>
2023-03-18 22:57:07 +0100

    refactor

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
-'''
-Script that takes in an argument and displays all values in the
-states table of hbtn_0e_0_usa where name matches the argument
-'''
+""" print states starting with capital N"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    connect = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3])
-    current = connect.cursor()
-    current.execute(
-        """SELECT * from states WHERE BINARY name='{}' ORDER by id ASC"""
-        .format(sys.argv[4])
-    )
-    query_rows = current.fetchall()
+    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                           user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    query = """ SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY
+    states.id ASC"""
+    query = query.format(argv[4])
+    cur.execute(query)
+    query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
-    current.close()
-    connect.close()
+    cur.close()
+    conn.close()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-'''
-Create State class and an instance Base = declarative_base(). Creates a
-link to the database
-'''
+""" State ORM object"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
-
 Base = declarative_base()
 
 
 class State(Base):
-    '''
-    Saste class extending Base =>declarative base
-    '''
-    __tablename__ = 'states'
-    id = Column(
-        Integer, primary_key=True, nullable=False,
-        unique=True, autoincrement=True
-    )
+    """ ORM class for state """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
