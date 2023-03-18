248f04b7cda138c91c4bafcb91adc8fe4bd1fc8c
George <georgeperfect4u@gmail.com>
2023-03-18 00:19:26 -0700

    get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+Script that lists all states from the database hbtn_0e_0_usa
+'''
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    connect = MySQLdb.connect(host="localhost",
+                              port=3306,
+                              user=sys.argv[1],
+                              passwd=sys.argv[2],
+                              db=sys.argv[3],
+                              charset="utf8")
+    current = connect.cursor()
+    current.execute("SELECT * FROM states ORDER by id ASC")
+    query_rows = current.fetchall()
+    for row in query_rows:
+        print(row)
+    current.close()
+    connect.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,5 @@
+##Python Object-Relational Mapping
+
+In this project,I will link two amazing worlds: Databases and Python!
+
+In the first part, I will use the module MySQLdb to connect to a MySQL database and execute SQL queries.
