a07dda9e47ce656d2cc8e03b75909381bee64a0d
georgeeset <georgeperfect4u@gmail.com>
2023-03-18 17:22:02 +0100

    First state model

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+Script that takes in the name of a state as an argument and lists all
+cities of that state, using the database hbtn_0e_4_usa
+'''
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    connect = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
+    current = connect.cursor()
+    current.execute("""SELECT cities.name FROM cities, states WHERE
+                    cities.state_id = states.id AND states.name = %s""",
+                    (argv[4],))
+
+    query_rows = current.fetchall()
+    NEW_list = []
+    for row in query_rows:
+        NEW_list.append(row[0])
+        print(', '.join(city[0] for city in query_rows))
+    current.close()
+    connect.close()
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+Create State class and an instance Base = declarative_base(). Creates a
+link to the database
+'''
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False,
+                unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
