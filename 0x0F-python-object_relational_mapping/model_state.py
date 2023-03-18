b675c96cddc86efd3a27b8769e031cc4740a4852
georgeeset <georgeperfect4u@gmail.com>
2023-03-18 22:49:50 +0100

    comment add to state class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,13 +6,19 @@ link to the database
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
 Base = declarative_base()
 
 
 class State(Base):
+    '''
+    Saste class extending Base =>declarative base
+    '''
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False,
-                unique=True, autoincrement=True)
+    id = Column(
+        Integer, primary_key=True, nullable=False,
+        unique=True, autoincrement=True
+    )
     name = Column(String(128), nullable=False)
