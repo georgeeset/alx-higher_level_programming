e7cbb38414e6209962514c2d871944e2dd7f08e6
georgeeset <georgeperfect4u@gmail.com>
2023-03-19 13:05:14 +0100

    adjustment to query

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.id == 2).first()
+    states = session.query(State).filter(State.id == 2).all()
     if states:
         states[0].name = "New Mexico"
         session.commit()
