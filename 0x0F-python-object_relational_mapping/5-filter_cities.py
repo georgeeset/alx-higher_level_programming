f5a47d9cafbb4bb0dce25a531ce4f255ea73d5ce
georgeeset <georgeperfect4u@gmail.com>
2023-03-19 23:19:47 +0100

    query adjusted

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -15,14 +15,13 @@ if __name__ == "__main__":
         passwd=argv[2],
         db=argv[3])
     current = connect.cursor()
-    current.execute("""SELECT cities.name FROM cities, states WHERE
-                    cities.state_id = states.id AND states.name = %s""",
-                    (argv[4]))
+    current.execute("""SELECT cities.id, cities.name, states.name
+        FROM cities JOIN states ON cities.state_id = states.id
+        WHERE states.name = '{}'""",
+                    (sys.argv[4]))
 
     query_rows = current.fetchall()
-    new_list = []
-    for row in query_rows:
-        new_list.append(row[0])
-    print(', '.join(city[0] for city in new_list))
+
+    print(', '.join(city[0] for city in query_rows))
     current.close()
     connect.close()
