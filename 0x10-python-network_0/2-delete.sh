20b3dc113bf18dd5e073967c4dfbf552f5e4a309
georgeeset <georgeperfect4u@gmail.com>
2023-03-31 12:00:36 +0100

    curl only methods

---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/env bash
+# Script that sends a delete req to URL passed as 1st arg + displays the body
+curl -s -X DELETE "$1"
---
+++ 0x10-python-network_0/3-methods.sh
@@ -0,0 +1,3 @@
+#!/bin/env bash
+# Script that takes in URL as arg, sends GET req to it, + displays body
+curl -s --head "$1" | grep Allow | cut --complement -d -f1
