d51b5bd14f3a2da68ef02f18dafe035857ef2073
georgeeset <georgeperfect4u@gmail.com>
2023-04-01 10:25:59 +0100

    methods list with curl command

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in URL as arg, sends GET req to it, + displays body
-curl -s --head "$1" | grep Allow | cut --complement -d -f1
+curl -s -I "$1" | grep Allow | cut -d' ' -f2-
