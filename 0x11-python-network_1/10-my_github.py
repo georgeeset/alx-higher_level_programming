af6588f4aa957776fe4488b49ec235c5b5eaca2a
georgeeset <georgeperfect4u@gmail.com>
2023-04-02 01:44:40 +0100

    my github id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+A script that takes your Github credentials (username and password) and uses the Github
+API to display your id
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    addr = "https://api.github.com/user"
+    auth = (argv[1], argv[2])
+    response = requests.get(url, auth=auth)
+    try:
+        print(response.json().get("id"))
+    except ValueError:
+        print("Not a valid JSON")
