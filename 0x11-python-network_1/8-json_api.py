546a1e3f335e8f09d421ae18f0bf7bdb3b8d6ad4
georgeeset <georgeperfect4u@gmail.com>
2023-04-02 01:37:11 +0100

    search api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+A Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    addr = "http://0.0.0.0:5000/search_user"
+    data = {"q": argv[1][0] if len(argv) > 1 else ""}
+    response = requests.post(addr, data=data)
+    try:
+        first = response.json()
+        if not first:
+            print("No result")
+        else:
+            print("[{}] {}".format(first.get("id"), first.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
