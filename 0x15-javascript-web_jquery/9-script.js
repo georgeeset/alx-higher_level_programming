9cb6bb625ee0bb4de1f50631dc338daa1d9289d7
georgeeset <georgeperfect4u@gmail.com>
2023-05-05 04:31:54 +0100

    say hello

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,7 @@
+//JavaScript script that fetches from
+// https://fourtonfish.com/hellosalut/?lang=fr and displays the value of
+// hello from that fetch in the HTML tag DIV#hello
+
+$.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data, status) {
+  $('div#hello').text(data.hello);
+});
