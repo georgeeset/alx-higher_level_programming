76363adcf3f62cd495a68243253de3aea8b2690f
georgeeset <georgeperfect4u@gmail.com>
2023-05-05 04:37:34 +0100

    text color updaed

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,8 @@
+// JavaScript script
+// that updates the text color of
+// the <header> element to red (#FF0000)
+
+window.addEventListener('load', function () {
+    const header = document.querySelector('header');
+    header.style.color = '#FF0000';
+});
