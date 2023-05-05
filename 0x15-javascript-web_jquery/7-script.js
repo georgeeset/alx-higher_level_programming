bb17644e0f25de0e4ebc88ebc5e146a2b8a4f239
georgeeset <georgeperfect4u@gmail.com>
2023-05-05 04:10:03 +0100

    star wars

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,7 @@
+// JavaScript script that fetches the character
+// name from this URL: 
+// https://swapi-api.alx-tools.com/api/people/5/?format=json
+
+$.get('https://swapi.co/api/people/5/?format=json', function (data, status) {
+  $('div#character').text(data.name);
+});
